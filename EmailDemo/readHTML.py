98a038ef3cde6e8928ba857a0e9602b7c19293a4
singularity014 <49594316+singularity014@users.noreply.github.com>
2020-04-26 14:51:22 +0200

    Delete readHTML.py

--- EmailDemo/readHTML.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-from flask import Flask
-
-
-fname = r"C:\Users\User\Documents\UipathDocs\GE\SurveyForm.html"
-HtmlFile = open(fname, 'r', encoding='utf-8')
-source_code = HtmlFile.read()
-print(source_code)
